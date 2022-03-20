dd50d0c21c626cf2cc90fb7b6aca52e22747fab7
Dennis Byington <dennisbyington@mac.com>
2022-03-19 21:20:38 -0500

    smoothed out

--- aStar_search.py
+++ aStar_search.py
@@ -16,7 +16,7 @@ from aStar_getPath import path  # return A* path
 
 
 # --------------------------------------------------
-def aStar_search(grid, start, stop):
+def aStar_search(grid, startPos, stopPos):
     """
     - accepts grid, start position, and goal position
     - performs A* search (steps are described in comments)
@@ -28,103 +28,133 @@ def aStar_search(grid, start, stop):
     rows, columns = np.shape(grid)
 
     # create start/stop nodes
-    startNode = Node(None, start)
-    startNode.f = startNode.g = startNode.h = 0
-    stopNode = Node(None, stop)
-    stopNode.f = stopNode.g = stopNode.h = 0
+    start = Node(None, startPos)
+    start.f = start.g = start.h = 0
+    goal = Node(None, stopPos)
+    goal.f = goal.g = goal.h = 0
 
-    # create open (heapq) and closed lists
+    # create open list (heapq) and closed list
     openList = []
     heapq.heapify(openList)
     closedList = []
 
-    # add startNode to open list
-    heapq.heappush(openList, startNode)
+    # add start node to open list
+    heapq.heappush(openList, start)
 
     # set possible moves
     # TODO: if flag set, add the four diagonal moves
-    moves = ((-1, 0), (1, 0), (0, -1), (0, 1))
+    moves = ((-1,  0),   # up
+             ( 1,  0),   # down
+             ( 0, -1),   # left
+             ( 0,  1))   # right
+
+    print(f'Beginning A* search...')
 
     # while open-list not empty
     while(openList):
-        # print current openList
-        print(f'-----------------------------------------')
-        print(f'openList: ', end="")
-        for n in openList:
-            print(f'{n.position}', end=", ")
-        print("")
-        #input("press enter to continue")
+        # ----------------------------------------------------------------------
+        # # print current openList
+        # print(f'-----------------------------------------')
+        # print(f'openList: ', end="")
+        # for n in openList:
+        #     print(f'{n.position}', end=", ")
+        # print("")
+        # #input("press enter to continue")
+        # ----------------------------------------------------------------------
 
         # pop lowest f-cost off the open-list / add to closed list
-        currentNode = heapq.heappop(openList)
-        closedList.append(currentNode)
-        print(f'\ncurrentNode: {currentNode.position}')
+        current = heapq.heappop(openList)
+        closedList.append(current)
 
-        # if target -> stop search
-        if currentNode == stopNode:
-             print("stop found.  returning...")
-             return path(currentNode)
+        # ----------------------------------------------------------------------
+        # print(f'\ncurrent: {current.position}')
+        # ----------------------------------------------------------------------
 
-        # list to hold nodes to check
+        # if goal -> stop search
+        if current == goal:
+            print("Goal found, returning...\n")
+            return path(current)
+
+        # list to hold nodes to check TODO: rename?
         nodesToCheck = []
 
         # check possible moves for barrier & inbounds --> if not, append to nodesToCheck
         for move in moves:
-            # get adjacent node to check
-            position = (currentNode.position[0] + move[0], currentNode.position[1] + move[1])
+            # get adjacent node to check (U, D, L, R)
+            position = (current.position[0] + move[0], current.position[1] + move[1])
 
             # if not walkable --> ignore
-            if (position[0] < 0 or
-            position[0] >= rows or
-            position[1] < 0 or
-            position[1] >= columns):
-                print(f'{position[0]:2} {position[1]:2} : not walkable')
+            if (position[0] < 0 or      # oob high
+            position[0] >= rows or      # oob low
+            position[1] < 0 or          # oob l
+            position[1] >= columns):    # oob r
+                # ----------------------------------------------------------------------
+                # print(f'{position[0]:2} {position[1]:2} : not walkable')
+                # ----------------------------------------------------------------------
                 continue
 
             # if barrier --> ignore
             if (grid[position] != 0):
-                print(f'{position[0]:2} {position[1]:2} : barrier')
+                # ----------------------------------------------------------------------
+                # print(f'{position[0]:2} {position[1]:2} : barrier')
+                # ----------------------------------------------------------------------
                 continue
 
             # # if here, walkable and in bounds -> create node and append to nodesToCheck
-            print(f'{position[0]:2} {position[1]:2} : adding to nodesToCheck')
-            newNode = Node(currentNode, position)
+            # ----------------------------------------------------------------------
+            # print(f'{position[0]:2} {position[1]:2} : adding to nodesToCheck')
+            # ----------------------------------------------------------------------
+            newNode = Node(current, position)
             nodesToCheck.append(newNode)
 
-        print(f'\nnodesToCheck: ', end = "")
-        for n in nodesToCheck:
-            print(f'{n.position}', end = ", ")
-        print("")
+        # ----------------------------------------------------------------------
+        # print(f'\nnodesToCheck: ', end = "")
+        # for n in nodesToCheck:
+        #     print(f'{n.position}', end = ", ")
+        # print("")
+        # ----------------------------------------------------------------------
 
-        # look at each nodeToCheck for closed list (ignore), not on open list (add), better path (update)
-        for nodeToCheck in nodesToCheck:
-            print(f'\ncurrent: nodeToCheck: {nodeToCheck.position}')
+        # look at each node for closed list (ignore), not on open list (add), better path (update)
+        for node in nodesToCheck:
+            # ----------------------------------------------------------------------
+            # print(f'\ncurrent: node: {node.position}')
+            # ----------------------------------------------------------------------
             # if in closed list --> ignore
-            if nodeToCheck in closedList:
-                print(f'{nodeToCheck.position[0]:2} {nodeToCheck.position[1]:2} : in closed list')
+            if node in closedList:
+                # ----------------------------------------------------------------------
+                # print(f'{node.position[0]:2} {node.position[1]:2} : in closed list')
+                # ----------------------------------------------------------------------
                 continue
 
             # populate node and check open list
-            nodeToCheck.parent = currentNode
+            node.parent = current
             # TODO: update this to account for diag moves @ 14 cost
-            nodeToCheck.g = currentNode.g + 10
-            nodeToCheck.h = getH(nodeToCheck, stopNode)
-            nodeToCheck.f = nodeToCheck.g + nodeToCheck.h
-            print(f'{nodeToCheck.position}: f = g + h: {nodeToCheck.f} = {nodeToCheck.g} + {nodeToCheck.h}')
+            node.g = current.g + 10
+            node.h = getH(node, goal)
+            node.f = node.g + node.h
+            # ----------------------------------------------------------------------
+            # print(f'{node.position}: f = g + h: {node.f} = {node.g} + {node.h}')
+            # ----------------------------------------------------------------------
 
             # if already in open list & current g is higher, ignore
-            if len([n for n in openList if nodeToCheck.position == n.position and nodeToCheck.g >= n.g]) > 0:
-                print(f'{nodeToCheck.position[0]:2} {nodeToCheck.position[1]:2} : in open list with lower g-cost')
+            if len([n for n in openList if node.position == n.position and node.g >= n.g]) > 0:
+                # ----------------------------------------------------------------------
+                # print(f'{node.position[0]:2} {node.position[1]:2} : in open list with lower g-cost')
+                # ----------------------------------------------------------------------
                 continue
 
             # if here, add to open list
-            heapq.heappush(openList, nodeToCheck)
-            print(f'{nodeToCheck.position[0]:2} {nodeToCheck.position[1]:2} : adding to open list')
+            heapq.heappush(openList, node)
+            # ----------------------------------------------------------------------
+            # print(f'{node.position[0]:2} {node.position[1]:2} : adding to open list')
+            # ----------------------------------------------------------------------
 
-        print(f'\nclosedList: ', end = "")
-        for n in closedList:
-            print(f'{n.position}', end = ", ")
-        print("")
+        # ----------------------------------------------------------------------
+        # print(f'\nclosedList: ', end = "")
+        # for n in closedList:
+        #     print(f'{n.position}', end = ", ")
+        # print("")
+        # ----------------------------------------------------------------------
 
     # if here while loop has broken -> openList is empty and goal is not found
     print(f'Error.  Goal not found.  No path available.')
