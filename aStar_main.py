89c0e04276b1ceb8a1a217ba0a1094cc3cea9b39
Dennis Byington <dennisbyington@mac.com>
2022-03-19 20:54:44 -0500

    smooth

--- aStar_main.py
+++ aStar_main.py
@@ -11,76 +11,82 @@ TODO: ------------------------------------------------------------
 
 all files
     TODO: run through linter and formatter
-    TODO: update all block quotes below function defs
     TODO: remove all debugging statements
     TODO: smooth variables names and flow -> make readable and my own
 
 aStar_main.py
     TODO: still need to update instructions, readme, and final report
-    TODO: eventually randomize the barriers
-    TODO: move argparse into separate .py file
-    TODO: add argparse options/flags
-    TODO: pretty print resulting maze (and visited nodes)
+    TODO: pretty print resulting maze (*and visited nodes*)
+    TODO: update all block quotes below function defs
 
-astar_search.py
-    TODO: update hueristic function to allow to alternate h-functions (ties into options/flags)
+aStar_args.py
+    TODO: add argparse options/flags
+    TODO: eventually randomize the barriers
+    
+aStar_search.py
     TODO: update moves to allow for diagonal moves (ties into options/flags && will require refactoring algo)
+    TODO: update block quotes below function def
+    TODO: fix error message not being reached if no path available...
+    
+aStar_getH.py
+    TODO: update heuristic function to allow to alternate h-functions (ties into options/flags)
+    
+aStar_getPath.py
+    none
 
 Node.py
-    none...
+    none
 
 TODO: ------------------------------------------------------------
 """
 
-import argparse
-import numpy as np
-from aStar_search import aStar_search as search
-
-# --------------------------------------------------
-def get_args():
-    """parse command line args"""
-
-    parser = argparse.ArgumentParser(
-        description='A* pathfinding',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    # Add options/flags:
-    # option to resrict movement to 4 or 8
-    # option to set hueristic: Dijkstra’s algo, Manhattan distance, or Diagonal distance
-    # option to adjust barrier % (this wasn't part of my proposal...)
-
-    return parser.parse_args()
-
+from aStar_args import get_args                     # parse command line args
+import numpy as np                                  # array
+from aStar_search import aStar_search as search     # aStar search function
 
 # --------------------------------------------------
 def main():
-    """main funct"""
+    """
+    - parses any command line args
+    - build the grid (including barriers)
+    - designate start and goal locations
+    - call search function (returns A* path or error message if no path)
+    - print out grid and resulting path
+    """
 
+    # get command line arguments
     args = get_args()
 
     # define the maze
-    grid = np.array([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                     [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0, 1, 1, 1, 0, 0],
-                     [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]])
+    grid = np.array([[0, -1, 0,  0, 0,  0,  0,  0, 0, 0],
+                     [0, -1, 0,  0, 0,  0,  0,  0, 0, 0],
+                     [0, -1, 0, -1, 0,  0,  0,  0, 0, 0],
+                     [0, -1, 0, -1, 0,  0,  0,  0, 0, 0],
+                     [0,  0, 0, -1, 0,  0,  0,  0, 0, 0],
+                     [0,  0, 0, -1, 0, -1, -1, -1, 0, 0],
+                     [0,  0, 0, -1, 0, -1,  0,  0, 0, 0],
+                     [0,  0, 0, -1, 0, -1,  0,  0, 0, 0],
+                     [0,  0, 0, -1, 0,  0,  0,  0, 0, 0],
+                     [0,  0, 0, -1, 0,  0,  0,  0, 0, 0]])
 
-    # print maze to verify
-    # print(str(grid).replace(' [', '').replace('[', '').replace(']', ''))
-
-    # set start and end
+    # set start and goal
     start = (0, 0)
-    stop = (9, 9)
+    goal = (9, 9)
 
-   # print(grid[start])
+    # call search function (returns path or error message)
+    path = search(grid, start, goal)
 
-    path = search(grid, start, stop)
-    print(f'aStar path = {path}')
+    # print(f'aStar path = {path}')
+
+    # loop through each position in path & assign that grid position a numerical path (1, 2, 3...goal)
+    for i, p in enumerate(path):
+        grid[p[0]][p[1]] = i + 1
+
+    # print maze
+    print(f'A* search results:')
+    print(f'   X indicates a barrier / 0 indicates an open node')
+    print(f'   Path begins at 1 and continues increasing until goal reached\n')
+    print(str(grid).replace(' [', '').replace('[', '').replace(']', '').replace('-1', ' X'))
 
 
 # --------------------------------------------------
