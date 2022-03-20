29092bd81af36fb487a954c7c76702b5cff791fe
Dennis Byington <dennisbyington@mac.com>
2022-03-19 20:57:30 -0500

    misc

--- aStar_getH.py
+++ aStar_getH.py
@@ -14,7 +14,7 @@ def getH(current, goal):
     - returns the calculated heuristic
     """
 
-    # Manhattan
+    # Manhattan TODO: add equation here...
     x = abs(current.position[0] - goal.position[0])
     y = abs(current.position[1] - goal.position[1])
     return 10 * (x + y)
